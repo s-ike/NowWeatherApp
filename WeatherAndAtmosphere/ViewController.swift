496268a4d46bf27cfc9f3c437bdc40b5f4320f3e
池田 俊太郎 <ikedashutarou@221x241x166x90.ap221.ftth.ucom.ne.jp>
2015-06-18 06:27:53 +0900

    渡すaddreDataの値が初期値になっていしまう

--- WeatherAndAtmosphere/ViewController.swift
+++ WeatherAndAtmosphere/ViewController.swift
@@ -7,43 +7,104 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    // 取得ラベル
-    var getButton = UIButton()
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    // 現在地の位置情報の取得にはCLLocationManagerを使用
+    var lm: CLLocationManager!
+    // 取得した緯度を保持するインスタンス
+    var latitude: CLLocationDegrees!
+    // 取得した経度を保持するインスタンス
+    var longitude: CLLocationDegrees!
+    // 地名
+    var addressData: String = ""
+    
+    var x = "0"
+    
+
+    @IBAction func getButton(sender: AnyObject) {
+        // フィールドの初期化
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        
+        // CLLocationManagerをDelegateに指定
+        lm.delegate = self
+        
+        // 位置情報取得の許可を求めるメッセージの表示．必須．
+        lm.requestAlwaysAuthorization()
+        // 位置情報の精度を指定．任意，
+        // lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
+        // lm.distanceFilter = 1000
+        
+        // GPSの使用を開始する
+        lm.startUpdatingLocation()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        // 取得ボタンのフレーム設定
-        getButton.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        // 取得ボタンの背景色
-        getButton.backgroundColor = UIColor.orangeColor()
-        // 取得ボタンの角丸
-        getButton.layer.masksToBounds = true
-        getButton.layer.cornerRadius = 100.0
-        // 取得ボタンのフォントと文字サイズ
-        getButton.titleLabel?.font = UIFont.systemFontOfSize(CGFloat(30))
-        // 取得ボタンのタイトルと色を設定：通常時
-        getButton.setTitle("取得", forState:UIControlState.Normal)
-        getButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        // 取得ボタンの色を設定：ボタンがハイライト
-        getButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Highlighted)
-        // 取得ボタンの位置を指定
-        getButton.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200)
-        // 取得ボタンのタグを設定
-        getButton.tag = 1
-        // 取得ボタンをViewに追加
-        self.view.addSubview(getButton)
-        
+        x = "2"
+        println("地名3：\(addressData)")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /* 位置情報取得成功時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
+        // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
+        latitude = newLocation.coordinate.latitude
+        // 取得した経度がnewLocation.coordinate.longitudeに格納されている
+        longitude = newLocation.coordinate.longitude
+        // 取得した緯度・経度をLogに表示
+        NSLog("latiitude: \(latitude) , longitude: \(longitude)")
+        
+        // get address
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error)->Void in
+            if error != nil {
+                println("Reverse geocoder failed with error" + error.localizedDescription)
+                return
+            }
+            if placemarks.count > 0 {
+                let pm = placemarks[0] as! CLPlacemark
+                self.displayLocationInfo(pm)
+                //stop updating location to save battery life
+                self.lm.stopUpdatingLocation()
+            } else {
+                println("Problem with the data received from geocoder")
+            }
+        })
+        
+        // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
+        lm.stopUpdatingLocation()
+        
+        // SegueのIDを引数にして画面遷移
+        performSegueWithIdentifier("weather", sender: nil)
     }
+    
+    /* 位置情報取得失敗時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        // この例ではLogにErrorと表示するだけ．
+        NSLog("Error")
+    }
+    
+    // 位置情報表示
+    func displayLocationInfo(placemark: CLPlacemark) {
+        var address: String = ""
+        address = placemark.administrativeArea != nil ? placemark.administrativeArea : ""
+        address += ","
+        address += placemark.country != nil ? placemark.country : ""
+        addressData = address
+                println("地名：\(addressData)")
+    }
+    
 
-
+    // 表示画面へ値を渡す
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let wVC = segue.destinationViewController as! WeatherViewController
+        println("地名2：\(addressData)")
+        wVC.addressData = addressData
+    }
+    
 }
 
